da1226be05f574ee1818071755c2640491a28112
구민석 <ser_om@naver.com>
2022-01-04 17:43:34 +0900

    01/04

--- src/com/javaex/ex01/Exapp.java
+++ src/com/javaex/ex01/Exapp.java
@@ -1,11 +1,20 @@
 package com.javaex.ex01;
 
+import java.util.Scanner;
 
 public class Exapp {
 	public static void main(String[] args)  {
 ////////////////////////////////////////////////////////
-
+		Scanner sc = new Scanner(System.in);
 		
+		System.out.println("점수: ");
+		int num = sc.nextInt();
+		System.out.println(num);
+		
+		
+		
+		
+		sc.close();
 		
 ////////////////////////////////////////////////////////
 	}
